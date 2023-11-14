346e7bf3d02db6373828b4a30ce30952c5b8b860
Daichi9764 <malbranc@insa-toulouse.fr>
2023-11-14 16:53:19 +0100

    main interface almost working

--- app/src/main/java/apphandicaped/UI/MainInterface.java
+++ app/src/main/java/apphandicaped/UI/MainInterface.java
@@ -7,6 +7,14 @@ import java.awt.event.*;
 
 public class MainInterface {
 
+    public static void changePanel(JFrame f, JPanel destPanel){
+        f.getContentPane().removeAll();
+        f.add(destPanel);
+        //reloaod frame
+        f.invalidate();
+        f.repaint();
+    }
+
     private static void createAndShowMainGUI() {
         // Create and set up the window.
         JFrame frame = new JFrame("main");
@@ -15,8 +23,8 @@ public class MainInterface {
         JLayeredPane layeredPane = new JLayeredPane();
 
         JPanel pMain = new JPanel(new GridLayout(2,1));
-        JPanel pLogin = new JPanel();
-        JPanel pRegister = new JPanel();
+        JPanel pLogin = new JPanel(new BorderLayout());
+        JPanel pRegister = new JPanel(new BorderLayout());
 
         JButton bLogin=new JButton("Login");
         JButton bRegister=new JButton("Register");
@@ -26,15 +34,26 @@ public class MainInterface {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
 
-        bLogin.addActionListener(new ActionListener(){  
-            public void actionPerformed(ActionEvent e){  
-                        //switch to Login page
-                    }  
-                });  
+        //basic panel for login and register
+        //affichage des panels login et register marche mal, il faut maximizer la fenetre pour voir les labels
+        //changePanel fonctionne correctement, et les labels sont corrects
+        JLabel loginLabel = new JLabel("Logging in");
+        JLabel registerLabel = new JLabel("Register proceding");
+        pLogin.add(loginLabel,BorderLayout.CENTER);
+        pRegister.add(registerLabel,BorderLayout.CENTER);
 
         bLogin.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){  
-                        //switch to Register page
+                    //switch to Login panel
+                    changePanel(frame,pLogin);
+
+                    }  
+                });  
+
+        bRegister.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                    //switch to Register panel
+                    changePanel(frame,pRegister);
                 }  
             }); 
 
@@ -42,10 +61,12 @@ public class MainInterface {
 
 
         frame.add(layeredPane);
-        layeredPane.add(pMain);
+        frame.add(pMain);
         pMain.add(bLogin);
         pMain.add(bRegister);
         frame.setVisible(true);
+
+        
     }
 
     public static void main(String[] args) {
