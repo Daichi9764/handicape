d29f71cac994fa09ac084f77a4351c35f2d2f523
ImadBKZZ <ied.boukezzata@gmail.com>
2023-11-19 20:42:32 +0100

    InterfaceSQL method

--- app/src/main/java/apphandicaped/Database/InterfaceMySQL.java
+++ app/src/main/java/apphandicaped/Database/InterfaceMySQL.java
@@ -2,6 +2,9 @@ package apphandicaped.Database;
 
 import java.sql.*;
 import java.util.ArrayList;
+
+import com.mysql.cj.protocol.Resultset;
+
 import apphandicaped.*;
 
 
@@ -27,9 +30,9 @@ public class InterfaceMySQL {
         Statement stm = conn.createStatement();
         ResultSet rs = stm.executeQuery("SELECT * from Users");
         while(rs.next()) {
-            System.out.print(rs.getString(1));
-            System.out.print(rs.getString(2));
-            System.out.print(rs.getString(3));
+            System.out.print( "Id : " + rs.getString(1) + " ");
+            System.out.print("FirstName : " + rs.getString(2) + " ");
+            System.out.print("LastName : " + rs.getString(3) + " ");
             System.out.println();
         }
 
@@ -42,6 +45,26 @@ public class InterfaceMySQL {
         stm.executeUpdate(SQLCommand);
 
     }
+
+    public static void delUser(Connection con , User user, int id) throws SQLException {
+        String SQLCommand = "DELETE FROM Users WHERE ID = " + id;
+        Statement stm = con.createStatement();
+        System.out.println(SQLCommand);
+        stm.executeUpdate(SQLCommand);
+    }
+
+    public static int getUserIDs(Connection con , User user) throws SQLException {
+
+        //a mettre Statement.RETURN_GENERATED_KEYS
+        String SQLCommand = "SELECT UserID FROM Users WHERE " + String.format("LastName = '%s' AND FirstName = '%s'", user.getLastName(), user.getFirstName());
+        Statement stm = con.createStatement();
+        System.out.println(SQLCommand);
+        ResultSet rs = stm.getGeneratedKeys();
+        if(rs.next()){
+            return rs.getInt(1);
+        }
+        return 0;
+    }
     
 
 
@@ -50,6 +73,10 @@ public class InterfaceMySQL {
         User user = new User("CR", "7");
         addUser(conn, user);
         displayUserTable(conn);
+
+        int id = getUserIDs(conn ,user);
+        System.out.println(id);
+
         conn.close();
         
         
