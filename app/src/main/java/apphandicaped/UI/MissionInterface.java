171cec89b04d135947876662fd5d67c7b57e6975
ImadBKZZ <ied.boukezzata@gmail.com>
2023-12-09 13:33:19 +0100

    amelioration LoginUI refonte MissionInterface

--- app/src/main/java/apphandicaped/UI/MissionInterface.java
+++ app/src/main/java/apphandicaped/UI/MissionInterface.java
@@ -3,45 +3,41 @@ package apphandicaped.UI;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableColumn;
 
 import apphandicaped.Database.InterfaceMySQL;
 
 import java.awt.*;
-import java.util.Date;  
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 import java.util.Vector;
 
 public class MissionInterface extends JPanel {
     private static JTable requestsTable;
-    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     Date now = new Date();
-    public MissionInterface() {
-        String[] columnNames = {"RequestID", "Description", "Date", "State", "Comment", "Help"};
+    private int CurrentHelperID;
+    private Timer timer;
+
+    public MissionInterface(int CurrentHelperID) {
+        this.CurrentHelperID = CurrentHelperID;
+
+        String[] columnNames = {"RequestID", "Description", "Date", "State", "Comment"};
         DefaultTableModel tableModel = new DefaultTableModel(null, columnNames) {
             @Override
             public boolean isCellEditable(int row, int column) {
-                return column == 5; // Rend la colonne "Help" éditable
+                return false; // Désactivez l'édition des cellules
             }
         };
 
         requestsTable = new JTable(tableModel);
-
-        // Personnalisez le rendu et l'éditeur de la colonne "Help"
-        TableColumn helpColumn = requestsTable.getColumnModel().getColumn(5);
-        helpColumn.setCellRenderer(new ButtonRenderer());
-        helpColumn.setCellEditor(new ButtonEditor(new JCheckBox()));
 
         JScrollPane scrollPane = new JScrollPane(requestsTable);
 
@@ -50,6 +46,59 @@ public class MissionInterface extends JPanel {
         add(scrollPane, BorderLayout.CENTER);
 
         // Chargez les données depuis la base de données
+        loadTableData();
+
+        // Ajoutez un écouteur de clic à la table
+        requestsTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int row = requestsTable.rowAtPoint(e.getPoint());
+                int col = requestsTable.columnAtPoint(e.getPoint());
+                if (row >= 0 && col >= 0) {
+                    // Affichez une popup pour la cellule sélectionnée
+                    try {
+                        showPopup(row, col);
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // Initialisez le Timer pour rafraîchir toutes les 3 secondes
+        timer = new Timer(3000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                refreshTableData();
+            }
+        });
+        timer.start();
+    }
+
+    private void showPopup(int row, int col) throws SQLException {
+        if (col == 0) { // Vérifiez si la colonne est celle des IDs de requête
+            Object requestID = requestsTable.getValueAt(row, col);
+            Object description = requestsTable.getValueAt(row, 1); // Colonne de description
+            Object comment = requestsTable.getValueAt(row, 4); // Colonne de commentaire
+    
+            int choice = JOptionPane.showConfirmDialog(this,
+                    "Voulez-vous accepter la mission avec l'ID de requête: " + requestID + "\n"
+                            + "Description: " + description + "\n"
+                            + "Commentaire: " + comment,
+                    "Confirmation d'acceptation de mission",
+                    JOptionPane.YES_NO_OPTION);
+    
+            if (choice == JOptionPane.YES_OPTION) {
+                InterfaceMySQL.requestAccepted(CurrentHelperID,((Integer) requestID).intValue());
+            }
+        }
+    }
+
+    private void refreshTableData() {
+        DefaultTableModel model = (DefaultTableModel) requestsTable.getModel();
+        model.setRowCount(0); // Effacez toutes les lignes existantes dans le modèle
+
+        // Chargez les nouvelles données depuis la base de données
         loadTableData();
     }
 
@@ -67,11 +116,10 @@ public class MissionInterface extends JPanel {
                     DefaultTableModel model = (DefaultTableModel) requestsTable.getModel();
                     Vector<Object> row = new Vector<>();
                     row.add(requestID);
-                    row.add("Pas de Description"); // Ajoutez une cellule vide pour la colonne "Description"
-                    row.add(formatter.format(now)); // Ajoutez une cellule vide pour la colonne "Date"
+                    row.add("Pas de Description");
+                    row.add(formatter.format(now));
                     row.add(requestStatus);
-                    row.add("Pas de commentaire"); // Ajoutez une cellule vide pour la colonne "Comment"
-                    row.add(""); // Ajoutez une cellule vide pour la colonne "Help"
+                    row.add("Pas de commentaire");
                     model.addRow(row);
                 }
             }
@@ -79,72 +127,16 @@ public class MissionInterface extends JPanel {
             e.printStackTrace();
         }
     }
-    private static boolean tableDisabled = false;
-
-    // Classe pour le rendu du bouton
-    static class ButtonRenderer extends DefaultTableCellRenderer {
-        
-        private JButton button;
-
-        public ButtonRenderer() {
-            button = new JButton("Help");
-            button.setOpaque(true);
-        }
-
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            return button;
-        }
-    }
-
-// Classe pour l'édition du bouton
-static class ButtonEditor extends AbstractCellEditor implements TableCellEditor, ActionListener {
-    private JButton button;
-    private boolean clicked;
-    private JTable table;
-
-    public ButtonEditor(JCheckBox checkBox) {
-        button = new JButton("Help");
-        button.addActionListener(this);
-        button.setOpaque(true);
-    }
-
-    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-        this.table = table;
-        clicked = false;
-        return button;
-    }
-
-    public Object getCellEditorValue() {
-        return clicked ? "Proceeding" : "Help";
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        int selectedRow = table.getSelectedRow();
-        if (selectedRow != -1 && !clicked) {
-            // Modifiez le texte de la cellule lorsqu'on clique sur le bouton
-            clicked = true;
-            button.setText("Proceeding");
-            button.setEnabled(false);  // Désactivez le bouton après le clic
-            fireEditingStopped();
-            disableHelpColumn();    // Désactivez la JTable
-        }
-        button.setEnabled(false);
-    }
-}
-private static void disableHelpColumn() {
-    // Supprimez l'éditeur de la cellule de la colonne "Help"
-    requestsTable.getColumnModel().getColumn(5).setCellEditor(null);
-}
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Mission Interface");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            MissionInterface missionInterface = new MissionInterface();
+            MissionInterface missionInterface = new MissionInterface(10);
             frame.add(missionInterface);
 
-            frame.setSize(1400, 800);
+            frame.setSize(800, 600);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
